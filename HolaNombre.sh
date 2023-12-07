5f0af9bed67e6ba4fa4f7b0a6e88b26addd9f021
juanalbertokuyoc <108863317+juanalbertokuyoc@users.noreply.github.com>
2023-12-06 19:01:50 -0500

    Add files via upload

---
+++ HolaNombre.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+clear
+
+Nombre=""
+
+echo -n "Ingresa tu nombre: "
+read Nombre
+
+echo -e "\033[40m\033[36m Este Script dice:  \033[0m"
+echo -e "\033[40m\033[35m \n¡Hola! ${Nombre} \033[0m"
